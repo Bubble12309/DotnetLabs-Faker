141ce319070a6025dc8ca38688523d17e77a4074
Konanau Aliaksandr <m.kononov.alexander@gmail.com>
2022-10-24 16:03:11 +0300

    [Faker Library] Add Point and string generators to be plugged

---
+++ PointGenerator/PointGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+namespace University.DotnetLabs.Lab2.FakerLibrary;
+
+public class PointGenerator : Generator
+{
+    Random random = new Random();
+
+    public int MaxX { get; } = 50;
+    public int MaxY { get; } = 50;
+    public override object? Generate()
+    {
+        return new Point(random.Next(0, MaxX), random.Next(0, MaxY));
+    }
+
+    public PointGenerator() : this(50, 50) { }
+    public PointGenerator(int maxX, int maxY)
+    {
+        GeneratingType = typeof(Point);
+        MaxX = maxX;
+        MaxY = maxY;
+    }
+}
---
+++ StringGenerator/StringGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using University.DotnetLabs.Lab2.FakerLibrary;
+
+namespace University.DotnetLabs.Lab2.StringGenerator;
+
+public class StringGenerator : Generator
+{
+    private static char[] _chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-=+~`'".ToCharArray();
+
+    Random random = new Random();
+    public override object? Generate()
+    {
+        var length = random.Next(1, 50);
+        var builder = new StringBuilder(length);
+        for (int i = 0; i < length; i++)
+        {
+            builder.Append(_chars[random.Next(_chars.Length)]);
+        }
+        return builder.ToString();
+    }
+
+    public StringGenerator() => GeneratingType = typeof(string);
+}
