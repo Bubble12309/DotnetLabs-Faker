358d4447fc8b3ae6856ee765bbb852941c020cb8
Konanau Aliaksandr <m.kononov.alexander@gmail.com>
2022-10-24 11:53:42 +0300

    [Faker Library] Add default object generator
    Returns new object()

---
+++ FakerLibrary/DefaultGenerators/ObjectGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace University.DotnetLabs.Lab2.FakerLibrary.DefualtGenerators;
+
+public sealed class ObjectGenerator : Generator
+{
+    Random random = new Random();
+    public override object Generate() => new object();
+}
